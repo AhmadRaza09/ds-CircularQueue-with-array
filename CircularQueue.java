6b757a5bf30b72f0c2f870cf4434ac8c4629b85d
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-08-16 22:27:35 +0500

    added enqueue method. closes #6.

--- CircularQueue.java
+++ CircularQueue.java
@@ -32,4 +32,32 @@ public class CircularQueue {
         return size == arr.length;
     }
 
+    /**
+     * This method will add the element in the queue
+     *
+     * @param value
+     * @throws throws exception if queue is full
+     */
+    public void enqueue(int value) throws Exception {
+        if (isFull()) {
+            throw new Exception("Queue is Full");
+        }
+
+        if (isEmpty()) {
+            beginningOfQueue = 0;
+        }
+
+        //if topOfQueue point to the last index of the queue then change it to the first index
+        if (topOfQueue == arr.length - 1) {
+            topOfQueue = -1;
+        }
+        topOfQueue = topOfQueue + 1;
+
+        //add element in the queue
+        arr[topOfQueue] = value;
+
+        //increase the size of the queue
+        size = size + 1;
+    }
+
 }
--- Main.java
+++ Main.java
@@ -1,9 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         CircularQueue circularQueue = new CircularQueue(5);
 
-        boolean result = circularQueue.isFull();
-
-        System.out.println(result);
+        System.out.println("--------------------- Enqueue");
+        circularQueue.enqueue(1);
+        circularQueue.enqueue(2);
+        circularQueue.enqueue(3);
+        circularQueue.enqueue(4);
+        circularQueue.enqueue(5);
+        circularQueue.enqueue(6);
     }
 }
