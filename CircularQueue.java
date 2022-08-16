8da008012ab0d4a109071d64b0bd544e72ca740d
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-08-16 21:44:18 +0500

    added data members in circularQueue class. closes #3.

--- CircularQueue.java
+++ CircularQueue.java
@@ -1,3 +1,13 @@
 public class CircularQueue {
+    private int[] arr;
+    private int topOfQueue;
+    private int beginningOfQueue;
+
+    public CircularQueue(int size) {
+        arr = new int[size];
+        topOfQueue = -1;
+        beginningOfQueue = -1;
+        System.out.println("Crated queue object");
+    }
 
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("made circular queue");
+        CircularQueue circularQueue = new CircularQueue(5);
     }
 }
