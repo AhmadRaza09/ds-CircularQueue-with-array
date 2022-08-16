fa9f3d581e0c6cc6161f4b396712a6e0c3455273
Ahmad Raza <ahmadrazamul09@gmail.com>
2022-08-16 21:51:11 +0500

    added isEmpty method. closes #4.

--- CircularQueue.java
+++ CircularQueue.java
@@ -10,4 +10,16 @@ public class CircularQueue {
         System.out.println("Crated queue object");
     }
 
+    /**
+     * This method will tell the queue is Empty or not
+     *
+     * @return boolean
+     */
+    public boolean isEmpty() {
+        if (beginningOfQueue == -1 && topOfQueue == -1) {
+            return true;
+        }
+        return false;
+    }
+
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         CircularQueue circularQueue = new CircularQueue(5);
+
+        boolean result = circularQueue.isEmpty();
+
+        System.out.println(result);
     }
 }
